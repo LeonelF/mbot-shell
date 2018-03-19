ed7af1d7e5efc61755387c41e4978c1cf6ed5b44
Leonel Faria <leonelf@gmail.com>
2018-03-19 11:08:44 +0000

    Added Castor Find Script for extra lulz

--- castor/castorf.py
+++ castor/castorf.py
@@ -8,9 +8,20 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 import urllib2
 
-url = 'https://maria.deadbsd.org/api/random'
+if (len(sys.argv) == 1):
+	print("Usage .castorf <text>")
+	sys.exit(0)
+text = sys.argv[1]
+try:
+	pos = sys.argv[2]
+except:
+	pos = 0
+
 tmp = ""
+count = 0
 while ("castor" not in tmp):
+	pos = int(pos)+count
+	url = 'https://maria.deadbsd.org/api/find?text={}&position={}'.format(text, pos)
 	try:
 		response = urllib2.urlopen(url)
 	except urllib2.URLError, e:
@@ -19,11 +30,15 @@ while ("castor" not in tmp):
 	values = json.loads(response.read())
 	try:
 		tmp = values['message']['text']
+		datetime = values['message']['datetime']
+		number = values['message']['number']
+		total = values['total']
+		nextfind = values['next']	
 	except:
 		print("No results found")
 		sys.exit(0)
 	tmp = re.sub("homem","castor",tmp, flags=re.I)
-    tmp = re.sub("homen","castor",tmp, flags=re.I)
+	tmp = re.sub("homen","castor",tmp, flags=re.I)
 	tmp = re.sub("senhoras ","castoras ",tmp, flags=re.I)
 	tmp = re.sub("senhora ","castora ",tmp, flags=re.I)
 	tmp = re.sub("senhoras","castoras",tmp, flags=re.I)
@@ -41,4 +56,6 @@ while ("castor" not in tmp):
 	tmp = re.sub("rapariga","castora",tmp, flags=re.I)
 	tmp = re.sub("mulher","castora",tmp, flags=re.I)
 	tmp = re.sub("menina","castora",tmp, flags=re.I)
-print("%s" % (tmp))
+	count += 1
+print("%d found '.castorf %s %s' for the next one" % (total, text, nextfind))
+print("%s - %s, %s " % (tmp, number, datetime))
